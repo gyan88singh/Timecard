f2b94db1ee57d819b0c09b067794d9e9c7239e33
gyan <gyanp8@gmail.com>
2016-10-12 19:11:12 +0530

    added capistrano files

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:gyan88singh/Timecard.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
- set :deploy_to, '/home/deploy/Timecard'
+ set :deploy_to, '/home/sysadmin/Timecard'
 
 # Default value for :scm is :git
 # set :scm, :git
